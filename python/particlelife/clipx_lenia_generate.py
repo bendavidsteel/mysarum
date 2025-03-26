03c40ae3ce1d8d1face109d03202b3d426d281e9
Ben Steel <bendavidsteel@gmail.com>
2025-03-26 14:16:54 -0400

    Added clipx lenia generator for embeddings:

---
+++ python/particlelife/clipx_lenia_generate.py
@@ -0,0 +1,142 @@
+import collections
+import functools
+import os
+
+import av
+import einops
+import imageio.v2 as iio
+import jax
+import jax.numpy as jp
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Slider, Button
+import numpy as np
+import polars as pl
+import torch
+from tqdm import tqdm
+import transformers
+
+from particle_lenia import step_f, Params, multi_step_scan, draw_particles, draw_multi_species_particles
+
+
+def to_storable(n_t):
+    d = n_t._asdict()
+    d = [{k: np.array(v[i]).tolist() for k, v in d.items()} for i in range(n_t.map_size.shape[0])]
+    return d
+
+@functools.partial(jax.jit, static_argnames=('num_particles', 'map_size', 'num_species', 'num_kernels', 'num_growth_funcs'))
+def generate_lenia_video(key, num_particles, map_size, num_species, num_kernels, num_growth_funcs):
+    # Initial parameters
+    
+    key, *subkeys = jax.random.split(key, 12)
+    mu_k = jax.random.uniform(subkeys[0], (num_species, num_species, num_kernels), minval=2.0, maxval=5.0)
+    sigma_k = jax.random.uniform(subkeys[1], (num_species, num_species, num_kernels), minval=0.5, maxval=3.0)
+
+    mu_g = jax.random.uniform(subkeys[3], (num_species, num_growth_funcs), minval=-2.0, maxval=2.0)
+    sigma_g = jax.random.uniform(subkeys[4], (num_species, num_growth_funcs), minval=0.05, maxval=1.0)
+
+    w_k = jax.random.uniform(subkeys[6], (num_species, num_species, num_kernels), minval=-0.1, maxval=0.1)
+    c_rep = jax.random.uniform(subkeys[7], (num_species, num_species), minval=0.1, maxval=2.0)
+
+    dt = 0.1
+    
+    params = Params(
+        mu_k=mu_k, 
+        sigma_k=sigma_k, 
+        w_k=w_k, 
+        mu_g=mu_g, 
+        sigma_g=sigma_g, 
+        c_rep=c_rep,
+        map_size=map_size
+    )
+    
+    num_dims = 2
+    key, *subkeys = jax.random.split(key, 3)
+    x = jax.random.uniform(subkeys[0], [num_particles, num_dims], minval=0, maxval=map_size)
+    species = jax.random.randint(subkeys[1], [num_particles], 0, num_species)
+
+    carry, trajectory = multi_step_scan(params, x, species, dt, 20000)
+
+    # draw trajectory
+
+    video = draw_multi_species_particles(trajectory, map_size, species, num_species, start=-8000, offset=1000)
+    return params, video
+
+def main():
+    write_video = False
+
+    key = jax.random.PRNGKey(7)
+
+    processor = transformers.AutoProcessor.from_pretrained("microsoft/xclip-base-patch32")
+    model = transformers.AutoModel.from_pretrained("microsoft/xclip-base-patch32")
+
+    batch_params = []
+    batch_embeddings = []
+    embed_path = './data/particle_lenia_embeddings.parquet.zstd'
+    embed_backup_path = './data/particle_lenia_embeddings_backup.parquet.zstd'
+    if not os.path.exists(embed_path):
+        df = pl.DataFrame()
+    else:
+        try:
+            df = pl.read_parquet(embed_path)
+        except:
+            df = pl.read_parquet(embed_backup_path)
+    pbar = tqdm()
+
+    batch_size = 10
+
+    while True:
+        key, *subkeys = jax.random.split(key, 6)
+        num_species = int(jax.random.randint(subkeys[0], (), 1, 8))
+        num_kernels = int(jax.random.randint(subkeys[1], (), 1, 3))
+        num_growth_funcs = int(jax.random.randint(subkeys[2], (), 1, 3))
+        num_particles = int(jax.random.choice(subkeys[3], jp.array([100, 200, 400]), ()))
+        map_size = 20
+
+        all_params, videos = jax.vmap(generate_lenia_video, in_axes=(0, None, None, None, None, None))(jax.random.split(key, batch_size), num_particles, map_size, num_species, num_kernels, num_growth_funcs)
+
+        if write_video:
+            # Choose which rendering method to use
+            # Process frames and save to video
+            print("Rendering frames...")
+            for video in videos:
+                file_path = './outputs/particle_lenia.mp4'
+                w = iio.get_writer(file_path, format='FFMPEG', mode='I', fps=30)#,
+                                #    codec='h264_vaapi',
+                                #    output_params=['-vaapi_device',
+                                #                   '/dev/dri/renderD128',
+                                #                   '-vf',
+                                #                   'format=gray|nv12,hwupload'],
+                                #    pixelformat='vaapi_vld')
+                for frame in np.array(video):
+                    w.append_data(frame)
+                w.close()
+
+        torch_videos = []
+        for video in videos:
+            torch_video = []
+            for image in video:
+                torch_video.append(torch.tensor(np.array(image)))
+            torch_videos.append(torch_video)
+
+        inputs = processor(videos=torch_videos, return_tensors="pt")
+
+        video_features = model.get_video_features(**inputs)
+
+        batch_params.extend(to_storable(all_params))
+        video_features = video_features.detach().numpy()
+        batch_embeddings.extend(list(video_features))
+
+        pbar.update(1)
+
+        if len(batch_params) == 10:
+            df = pl.concat([df, pl.from_dict({'params': batch_params, 'embedding': batch_embeddings})], how='diagonal_relaxed')
+            batch_params = []
+            batch_embeddings = []
+
+        df.write_parquet(embed_path)
+        if len(df) % 100 == 0:
+            df.write_parquet(embed_backup_path)
+
+if __name__ == '__main__':
+    main()
